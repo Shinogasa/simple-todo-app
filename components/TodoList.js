ac20159e8bc32c1cd86bc7a10260898e964bf97f
Shinogasa <cuulbulu@gmail.com>
2017-10-15 14:25:01 +0900

    change components

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,32 +1,32 @@
 import React, { PropTypes } from 'react';
-import Todo from './todo';
+import Todo from './Todo';
 
-//todolistの実態は<ul></ul>
-//リストの中の<li></li>はTodoコンポーネント
+// TodoListの実体は<ul>~</ul>
+// リストの中の<li>~</li>はTodoコンポーネントを使用している
 class TodoList extends React.Component {
-    render() {
-        return (
-            <ul>
-                {this.props.todos.map(todo =>
-                    <Todo
-                        key={todo.id}
-                        {...todo}
-                        onClick={() => this.props.onTodoClick(todo.id)}
-                    />
-                )}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map(todo =>
+          <Todo
+            key={todo.id}
+            {...todo}
+            onClick={() => this.props.onTodoClick(todo.id)}
+          />
+        )}
+      </ul>
+    );
+  }
 }
 
-//制約の指定
+// 制約の指定
 TodoList.propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        completed: PropTypes.bool.isRequired,
-        text: PropTypes.string.isRequired
-    }).isRequired).isRequired,
-    onTodoClick: PropTypes.func.isRequired
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  onTodoClick: PropTypes.func.isRequired
 };
 
 export default TodoList;
