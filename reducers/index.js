c613b9da45f08a5abd5e163f00fce5ebd371b444
Shinogasa <cuulbulu@gmail.com>
2017-10-15 14:18:08 +0900

    change reducers/index.js

--- reducers/index.js
+++ reducers/index.js
@@ -1,62 +1,57 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
 
-//1つ1つのtodoを処理するための関数
+// 一つ一つのTODOを処理するための関数（todosから利用されます）
 const todo = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            };
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
 
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
-
-            return Object.assign({}, state, {
-                completed: !state.completed
-            });
-
-        default:
-            return state;
-    }
+      return Object.assign({}, state, {
+        completed: !state.completed
+      });
+    default:
+      return state;
+  }
 }
 
-//複数のtodoを処理するための関数
-const todos =(state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
-
-        case 'TOGGLE_TODO':
-            return state.map(t =>
-                todo(t, action)
-            );
-
-        default:
-            return state;
-    }
+// 複数のTODOを処理するための関数
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        todo(undefined, action)
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(t =>
+        todo(t, action)
+      );
+    default:
+      return state;
+  }
 }
 
-//todoの表示状態を処理するための関数
-const visibilityFilter = (state ='SHOW_ALL', action) => {
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter;
-
-        default:
-            return state;
-    }
+// TODOの表示状態を処理するための関数
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
 }
 
-//上の関数をまとめて公開
+// 上記の関数をまとめて外に公開する
 const todoApp = combineReducers({
-    todos,
-    visibilityFilter
-})
+  todos,
+  visibilityFilter
+});
 export default todoApp;
